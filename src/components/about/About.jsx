5f2737c5ffe08b7a49e61273b1486b767a8bbf99
Stone Comstock <comstockstone@yahoo.com>
2022-05-14 22:06:07 -0700

    finished about component, started project components

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,7 +11,15 @@ const About = () => {
             <img src={Laptop} alt="" className="about-img" />
         </div>
       </div>
-      <div className='about-right'></div>
+      <div className='about-right'>
+        <h1 className="about-title">About Me</h1>
+        <p className="about-sub">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ea et ut obcaecati. 
+        </p>
+        <p className="about-desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ipsam esse nihil porro quo minus vel, quaerat beatae aliquam sed perferendis tempora quae adipisci, rem distinctio molestias aliquid officia animi?
+        </p>
+      </div>
     </div>
   );
 };
