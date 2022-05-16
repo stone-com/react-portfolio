e60a0278df3420f417804627204bf5f615de52a4
Stone Comstock <comstockstone@yahoo.com>
2022-05-15 20:30:12 -0700

    Working on contact component still

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import './contact.css';
+import Phone from '../../img/phone.png';
+import Email from '../../img/email.png';
+import Address from '../../img/address.png';
 
 const Contact = () => {
-  return <div className='contact'>
-      <div className="contact-background">
-          <div className="contact-wrapper">
-              <div className="contact-left"></div>
-              <div className="contact-right"></div>
+  return (
+    <div className='contact'>
+      <div className='contact-background'></div>
+      <div className='contact-wrapper'>
+        <div className='contact-left'>
+          <h1 className='contact-title'>Contact me at any time!</h1>
+          <div className='contact-info'>
+            <div className='contact-info-item'>
+              <img src={Phone} alt='' className='contact-icon' />
+              +1-360-628-2047
+            </div>
+            <div className='contact-info-item'>
+              <img src={Email} alt='' className='contact-icon' />
+              comstockstone@yahoo.com
+            </div>
+            <div className='contact-info-item'>
+              <img src={Address} alt='' className='contact-icon' />
+              Olympia, WA, 98501
+            </div>
           </div>
+        </div>
+        <div className='contact-right'></div>
       </div>
-  </div>;
+    </div>
+  );
 };
 
 export default Contact;
